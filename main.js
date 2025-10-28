f9fa8c85004dc87ca3cce8e18686eaff289f286c
orrans <orrans@gmail.com>
2025-10-28 16:19:31 +0200

    Increase ball size

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function onBallClick() {
-    console.log('The ball says "Click!"')
+function onBallClick(elBall) {
+    var currSize = parseInt(getComputedStyle(elBall).width)
+    currSize += 50
+
+    elBall.style.width = currSize + 'px'
+    elBall.style.height = currSize + 'px'
+
+    elBall.textContent = currSize
 }
