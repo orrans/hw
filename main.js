5b12437b99e427589e857070f1b8d9ccaf114397
orrans <orrans@gmail.com>
2025-10-28 16:28:32 +0200

    Limit ball size to 400

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+let isAnimating = false
+
 function onBallClick(elBall) {
+    if (isAnimating) return
+    isAnimating = true
+
     var currSize = parseInt(getComputedStyle(elBall).width)
-    currSize += 50
+    currSize = currSize >= 400 ? 100 : currSize + 50
 
     elBall.style.width = currSize + 'px'
     elBall.style.height = currSize + 'px'
 
     elBall.textContent = currSize
+
+    setTimeout(() => {
+        isAnimating = false
+    }, 500)
 }
