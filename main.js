3cf3885ced51eedfb531a0dc15b2492dec7a6c42
orrans <orrans@gmail.com>
2025-10-28 16:53:53 +0200

    Change ball color randomly

--- main.js
+++ main.js
@@ -7,14 +7,18 @@ function onBallClick(elBall) {
     var currSize = parseInt(getComputedStyle(elBall).width)
     // currSize = currSize >= 400 ? 100 : currSize + 50
     var randomSize = getRandomNumInclusive(20, 60)
+    var randomColor = getRandomColor()
+
     currSize += randomSize
-    
+
     if (currSize >= 400) currSize = 100
 
     elBall.style.width = currSize + 'px'
     elBall.style.height = currSize + 'px'
+    elBall.style.backgroundColor = randomColor
 
     elBall.textContent = currSize
+    
 
     setTimeout(() => {
         isAnimating = false
