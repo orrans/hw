3a35f2c9e72b4ed2ab6bad83ecde181a9cd54c01
orrans <orrans@gmail.com>
2025-10-28 17:00:55 +0200

    Add maxDiameter param to onBallClick

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 let isAnimating = false
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
     if (isAnimating) return
     isAnimating = true
 
     var currSize = parseInt(getComputedStyle(elBall).width)
-    // currSize = currSize >= 400 ? 100 : currSize + 50
     var randomSize = getRandomNumInclusive(20, 60)
     var randomColor = getRandomColor()
 
     currSize += randomSize
 
-    if (currSize >= 400) currSize = 100
+    if (currSize >= maxDiameter) currSize = 100
 
     elBall.style.width = currSize + 'px'
     elBall.style.height = currSize + 'px'
     elBall.style.backgroundColor = randomColor
 
     elBall.textContent = currSize
-    
 
     setTimeout(() => {
         isAnimating = false
