48842cefda58024725cbc073bb849f1b221a6303
orrans <orrans@gmail.com>
2025-10-28 16:51:53 +0200

    Increase ball size by random number

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ function onBallClick(elBall) {
     isAnimating = true
 
     var currSize = parseInt(getComputedStyle(elBall).width)
-    currSize = currSize >= 400 ? 100 : currSize + 50
+    // currSize = currSize >= 400 ? 100 : currSize + 50
+    var randomSize = getRandomNumInclusive(20, 60)
+    currSize += randomSize
+    
+    if (currSize >= 400) currSize = 100
 
     elBall.style.width = currSize + 'px'
     elBall.style.height = currSize + 'px'
